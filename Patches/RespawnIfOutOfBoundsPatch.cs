729ce37402e8bbaecf47bfdf3d80dfec74d86807
kaycodes13 <egad.coding@gmail.com>
2026-01-19 21:26:41 -0500

    Fixes #10: force a respawn if hornet is above scene borders

---
+++ Patches/RespawnIfOutOfBoundsPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace VVVVVV.Patches;
+
+[HarmonyPatch(typeof(HeroController), nameof(HeroController.Update10))]
+internal static class RespawnIfOutOfBoundsPatch {
+
+	private static void Postfix(HeroController __instance) {
+		if (
+			GameManager.SilentInstance is GameManager gm && gm.IsGameplayScene()
+			&& V6Plugin.GravityIsFlipped
+			&& __instance.transform.position.y > gm.sceneHeight + 20
+		) {
+			V6Plugin.QueueRespawnHero();
+		}
+	}
+
+}
